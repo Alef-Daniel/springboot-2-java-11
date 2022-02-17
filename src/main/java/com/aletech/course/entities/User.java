3882dbcca803592ce69115fd556c90873431d239
Alef Sousa Aguiar Daniel <alefsa_daniel@hotmail.com>
2022-02-17 20:34:17 -0300

    Service layer, component registration

--- src/main/java/com/aletech/course/entities/User.java
+++ src/main/java/com/aletech/course/entities/User.java
@@ -1,16 +1,22 @@
 package com.aletech.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 @Entity
+@Table(name="tb_user")
 public class User implements Serializable{
 	
 
@@ -22,6 +28,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	
 	public User() {
@@ -87,6 +96,13 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
+	
 
 
 	@Override
@@ -106,6 +122,9 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
+	
 	
 	
 	
---
+++ src/main/java/com/aletech/course/services/OrderService.java
@@ -0,0 +1,29 @@
+package com.aletech.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aletech.course.entities.Order;
+import com.aletech.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+@Autowired	
+private OrderRepository repository;
+
+
+public List<Order> findAll(){
+	return repository.findAll();
+}
+
+public Order findById(Long id){
+	Optional<Order> obj=repository.findById(id);
+	return obj.get();
+}
+	
+	
+}
