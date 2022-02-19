3f00a0c565c771ae63537320101bcbfec41eb990
Alef Sousa Aguiar Daniel <alefsa_daniel@hotmail.com>
2022-02-18 21:36:05 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/aletech/course/entities/Order.java
+++ src/main/java/com/aletech/course/entities/Order.java
@@ -17,7 +17,6 @@ import javax.persistence.Table;
 
 import com.aletech.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_order")
